c3718edc72aec616c46172b7087bbe9bfecbd623
Programsko Inzenjerstvo <pi_foi@outlook.com>
2024-03-14 16:19:47 +0100

    Pocetak2

--- app/src/main/java/hr/foi/rmai/memento/MainActivity.kt
+++ app/src/main/java/hr/foi/rmai/memento/MainActivity.kt
@@ -5,10 +5,29 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayout.Tab
+import com.google.android.material.tabs.TabLayoutMediator
+import hr.foi.rmai.memento.adapters.MainPagerAdapter
 
 class MainActivity : AppCompatActivity() {
+    lateinit var tabLayout : TabLayout
+    lateinit var viewPager : ViewPager2
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tabLayout = findViewById(R.id.tabs)
+        viewPager = findViewById(R.id.viewpager)
+
+        val mainPagerAdapter = MainPagerAdapter(supportFragmentManager, lifecycle)
+        viewPager.adapter = mainPagerAdapter
+
+        TabLayoutMediator(tabLayout, viewPager) {tab, position ->
+            tab.setText(mainPagerAdapter.fragmentItems[position].titleRes)
+            tab.setIcon(mainPagerAdapter.fragmentItems[position].iconRes)
+        }.attach()
     }
 }
